53feb9dfd3dd88961b8539148887d9d83c5a7668
Michał Karnath <karnath.michal@gmail.com>
2021-05-27 13:18:37 +0200

    styles added to project

--- script.js
+++ script.js
@@ -4,39 +4,18 @@ const selectSecond = document.querySelector(".select__convert");
 const output = document.querySelector(".result")
 const API_KEY = "a1e96406548584d60eae8427f139669f"
 
-output.value = 1
 
 class CurrencyCalculator {
 
-    dataFetched() {
-        fetch("http://api.exchangeratesapi.io/v1/latest?access_key=a1e96406548584d60eae8427f139669f")
-        .then((response) => response.json())
-        .then((data) => {
-            const rates = data.rates
-            const dataKeys = Object.keys(rates)
-            this.createOptionElement(dataKeys, selectFirst);
-            this.createOptionElement(dataKeys, selectSecond)
-            output.innerText = `Result : ${output.value} ${selectSecond.value}`
-            input.addEventListener('keyup', (e) => this.calculation(e, rates))
-            selectFirst.addEventListener('change', (e) => this.calculation(e, rates))
-            selectSecond.addEventListener('change', (e) => this.calculation(e, rates))
-        })
-        .catch(() => {
-            console.error('Nie udało sie pobrać danych')
-        })
-    }
-
-    
-    createOptionElement(dataKeys, el) {
-        dataKeys.map((key) => {
+    createOptionElement(data, el) {
+        data.map((key) => {
         const option = document.createElement('option')
         option.innerHTML = key;
-        el.appendChild(option)
+        el.appendChild(option);
         })
     }
 
-    calculation(e, rates){
-        e.preventDefault();
+    calculation(rates){
         if (input.value > 0) {    
             output.value = Math.round(input.value * rates[selectSecond.value] / rates[selectFirst.value] * 1000) / 1000
             output.innerText = `Result : ${output.value} ${selectSecond.value}`
@@ -46,9 +25,31 @@ class CurrencyCalculator {
         
     }
 
-    
+    dataFetched() {
+        fetch("http://api.exchangeratesapi.io/v1/latest?access_key=a1e96406548584d60eae8427f139669f")
+        .then((response) => response.json())
+        .then((data) => {
+            const rates = data.rates
+            const dataKeys = Object.keys(rates)
+            this.createOptionElement(dataKeys, selectFirst);
+            this.createOptionElement(dataKeys, selectSecond)
+            output.innerText = `Result : ${output.value ? output.value : input.value} ${selectSecond.value}`
+            input.addEventListener('keyup', () => this.calculation(rates))
+            selectFirst.addEventListener('change', () => this.calculation(rates))
+            selectSecond.addEventListener('change', () => this.calculation(rates))
+            
+        })
+        .catch(() => {
+            console.error('Nie udało sie pobrać danych')
+        })
+    }
+
+    init() {
+        this.dataFetched()
+    }
 }
 
 const calc = new CurrencyCalculator();
-calc.dataFetched()
+calc.init()
+
 
